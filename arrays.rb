af1783ec7147338ae92a07a504c2f129fe380f9a
Leo Banos Garcia <bsleo625@gmail.com>
2022-08-25 18:15:31 -0600

    Added 4 arrays

---
+++ arrays.rb
@@ -0,0 +1,4 @@
+fruits = ["Banana", "Apple", "Pineapple", "Dragon Fruit", "Cherry", "Mango"]
+prices = [4.20, 7.83, 87.94, 31.57, 24.61, 2.49, 100.20]
+quantities = [1, 5, 7, 2, 75, 3, 32]
+availability = [true, false, false, false, true, false, true]
